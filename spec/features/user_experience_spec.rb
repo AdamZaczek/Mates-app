d9832fb40ad3c52547899bf4d1818f23ddc8e8ea
Adam Żaczek <adamzaczekul@gmail.com>
2016-03-25 00:27:22 +0000

    Add Factories

--- spec/features/user_experience_spec.rb
+++ spec/features/user_experience_spec.rb
@@ -1,6 +1,7 @@
+require "rails_helper"
 require "spec_helper"
 
-describe "page behavior for logged users" do
+describe "page behavior for not logged in visitors" do
   it "does not let strangers come inside" do
     visit "/"
     expect(page).to have_content("You need to sign in or sign up before continuing.")
@@ -9,7 +10,7 @@ end
 
 
 
-describe "page behavior for logged users" do  
+describe "simple page behavior for logged users" do  
   before :each do    
     visit "/users/sign_up"
   
@@ -32,9 +33,34 @@ describe "page behavior for logged users" do
     expect(page).to have_content("Add a group!")
   end
   
-  it "shows create group page" do
+  it "allows to create a new group" do
     visit "/groups/new"
+    
+    fill_in "Group name",                 :with => "skate contest"
+    
+    click_button "Create Group"
 
-    expect(page).to have_content("Add a group!")
+    expect(page).to have_content("Add new members to: skate contest")
+  end
+  
+  it "logouts" do
+    click_link "Logout"
+    
+    expect(page).to have_content("You need to sign in or sign up before continuing.")    
+  end
+end
+  
+  
+describe "user and group interactions" do  
+  before :each do   
+    visit "/"
+    user = FactoryGirl.create(:user)
+    login_as(user, :scope => :user)
+  end
+  
+  it "to be filled" do
+    visit "/"
+    
+    expect(page).to have_content("Your groups")
   end
 end
